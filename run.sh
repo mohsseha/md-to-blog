b51d85789cf3688b162b5c61fe6a5e9ef8b3ed4e
Husain <mohsseha@users.noreply.github.com>
2020-08-20 16:43:24 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+curl -sSL https://sdk.cloud.google.com | bash
+export PATH=$PATH:/root/google-cloud-sdk/bin
+
 python3 -m pip install -r requirements.txt --no-cache-dir
 
 python3 build_blog.py
