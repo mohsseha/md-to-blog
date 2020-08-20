592cf8262e670598c06c1066c2b5ec100ea308e9
Husain <mohsseha@users.noreply.github.com>
2020-08-20 17:16:53 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -sSL https://sdk.cloud.google.com | bash
-export PATH=$PATH:/root/google-cloud-sdk/bin
+#curl -sSL https://sdk.cloud.google.com | bash
+#export PATH=$PATH:/root/google-cloud-sdk/bin
 
-python3 -m pip install -r requirements.txt --no-cache-dir
+#python3 -m pip install -r requirements.txt --no-cache-dir
 
 python3 build_blog.py
 
-/root/google-cloud-sdk/bin/gsutil -m rsync -r -d out gs://husain.io/
+gsutil -m rsync -r -d out gs://husain.io/
