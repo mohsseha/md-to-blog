2c9f6738d7f6440e384abccfd90834bf36f4e3d2
Husain Al-Mohssen <husain@alum.mit.edu>
2020-08-24 09:36:49 -0400

    debug

--- run.sh
+++ run.sh
@@ -3,3 +3,9 @@
 # docker entry point to build blog and copy over:
 
 python3 build_blog.py && gsutil -m rsync -r -d -c out gs://husain.io/ || echo "👹gsutils failed👹"
+
+echo START DEBUG
+find / 
+echo END DEBUG
+
+echo syncing of out folder worked
