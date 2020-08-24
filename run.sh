ca4dc2ee06003fe752a20eb8678767fb5a86cc88
Husain Al-Mohssen <husain@alum.mit.edu>
2020-08-24 10:43:53 -0400

    clean up after finding the GCP bug, sad

--- run.sh
+++ run.sh
@@ -6,16 +6,4 @@ set -e
 
 
 python3 build_blog.py 
-echo finished building out
-echo here is outs content: 
-find out
-
-
 gsutil -m rsync -r -d -c out gs://husain.io/ 
-echo synced with gs bucket
-
-echo START DEBUG
-find out
-echo END DEBUG
-
-echo syncing of out folder worked
