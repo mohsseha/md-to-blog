e5fdc048b2088e6e5d5fda5f5074d095b681c5d4
Husain Al-Mohssen <husain@alum.mit.edu>
2020-08-24 09:38:33 -0400

    debug

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 python3 build_blog.py && gsutil -m rsync -r -d -c out gs://husain.io/ || echo "👹gsutils failed👹"
 
 echo START DEBUG
-find / 
+find /md-to-blog 
 echo END DEBUG
 
 echo syncing of out folder worked
